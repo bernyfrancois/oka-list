c51f9a863711ecf19d4d0310739e5e19d2261e82
bernyfrancois <itoutouberny@gmail.com>
2020-03-23 14:29:47 +0100

    Home page configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -9,14 +10,11 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    const primaryColor = Color(0xffFBCB3B);
-    const secondaryColor = Color(0xff0A111E);
-    const bgColor = Color(0xffF2F1ED);
     // TODO: implement build
     return new MaterialApp(
       title: 'Oka List',
       theme: new ThemeData(
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.amber,
       ),
       debugShowCheckedModeBanner: false,
       home: new Home(),
@@ -35,9 +33,57 @@ class Home extends StatefulWidget{
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    const primaryColor = Color(0xffFBCB3B);
+    const secondaryColor = Color(0xff0A111E);
+    const bgColor = Color(0xffF2F1ED);
     // TODO: implement build
     return Scaffold(
-      backgroundColor: Color(0xffFBCB3B),
+      backgroundColor: primaryColor,
+      appBar: new AppBar(
+        title: new Text("Oka List"),
+        centerTitle: true,
+        elevation: 20.0,
+        leading: new Icon(Icons.home),
+      ),
+      body: new Container(
+        alignment: Alignment.center,
+        child: Column(
+          children: <Widget>[
+            new Card(
+              color: bgColor,
+              elevation: 10.0,
+              child: new Container(
+                width: MediaQuery.of(context).size.width / 1.5,
+                height: 150.0,
+                child: Center(
+                    child: new Text(
+                      "Hello from card",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: secondaryColor,
+                        fontSize: 20,
+                      ),
+                    )
+                ),
+              ),
+            ),
+            new Container(
+              height: 150.0,
+              width: 150.0,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+              child: new Image.asset(
+                  'assets/img/avatr.png',
+                  fit: BoxFit.cover,
+              ),
+            ),
+            new Container(
+              width: 150.0,
+              height: 150.0,
+              child: new Image.asset("assets/img/icon.png")
+            )
+          ],
+        )
+      ),
     );
   }
 }
